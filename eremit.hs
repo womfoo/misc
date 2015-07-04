84eb3c5cbff13c66e3def4f23e54c2947f7787dc
Kranium Gikos Mendoza <kranium@gikos.net>
2015-07-05 05:40:24 +0800

    track newer url, drop http-conduit-browser and use dom-selector

--- eremit.hs
+++ eremit.hs
@@ -1,23 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 import Control.Applicative
 import Data.Attoparsec.Text
 import Data.Either
-import Data.Text.Lazy
+import Data.Text.Lazy (toStrict)
 import Data.Text.Format
 import Data.Text.Encoding
+import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Network.HTTP.Conduit
-import Network.HTTP.Conduit.Browser
 import Text.HTML.DOM as HTML
 import Text.XML.Cursor
+import Text.XML.Scraping (innerText)
+import Text.XML.Selector.TH
 
-data Currency = BDT | IDR | INR | LKR | PHP deriving Show
+data Currency = BDT | IDR | INR | LKR | MMK | NPR | PHP | PKR | SGD | VND deriving Show
 
 currencyParser = string "BDT" *> pure BDT
              <|> string "IDR" *> pure IDR
              <|> string "INR" *> pure INR
              <|> string "LKR" *> pure LKR
+             <|> string "MMK" *> pure MMK
+             <|> string "NPR" *> pure NPR
              <|> string "PHP" *> pure PHP
+             <|> string "PKR" *> pure PKR
+             <|> string "SGD" *> pure SGD
+             <|> string "VND" *> pure VND
 
 currPair :: Parser (Currency,Double)
 currPair = do
@@ -27,18 +35,14 @@ currPair = do
   return (curr,amount)
 
 main = do
-  request <- parseUrl "http://www.eremit.com.my/"
+  request <- parseUrl "http://www.eremit.com.my/rates"
   (date,bs) <- withManager $ \manager -> do
-    browse manager $ do
-      setMaxRetryCount 10
-      Response _ _ headers body <- makeRequestLbs request
-      return (lookup "Date" headers,body)
-  let rawrates = fromDocument (HTML.parseLBS bs)
-               $// element "div"
-               >=> attributeIs "id" "exchange"
-               >=> element "div"
-               &// content
-      rates = rights $ Prelude.map (parseOnly currPair) rawrates
+    response <- httpLbs request manager
+    return (lookup "Date" (responseHeaders response),responseBody response)
+  let doc = (fromDocument . parseLBS) bs
+      dropCommas = T.filter (/= ',')
+      rawrates = queryT [jq| #exchange > div > font |] doc
+      rates = rights $ Prelude.map (parseOnly currPair . dropCommas . toStrict . innerText) rawrates
   writeRates date rates
 
 writeRates (Just d) xs =
